bff67e966dbb83a45aad4f978e4e2923bc23c72a
Jonathan Wu <jonathan.wu3@outlook.com>
2020-12-02 15:26:03 -0500

    Day 2

--- Day2_2020.java
+++ Day2_2020.java
@@ -1,40 +1,49 @@
 import java.util.*;
 import java.io.*;
-public class Day1 {
+
+public class Day2_2020 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer in;
+
     public static void main(String[] args) throws IOException {
-        int total = 0, total2 = 0;
-        for (int i = 0; i < 100; i++) {
-            int tmp = readInt();
-            total += tmp / 3;
-            tmp = tmp / 3 - 2;
-            while (tmp > 0) {
-                total2 += tmp;
-                tmp = tmp / 3 - 2;
-            }
+        int valid = 0, valid2 = 0;
+        for (int i = 0; i < 1000; i++) {
+            String[] tmp = next().split("-");
+            int min = Integer.parseInt(tmp[0]), max = Integer.parseInt(tmp[1]);
+            byte letter = (byte) readChar();
+            byte[] str = next().getBytes();
+            int count = 0;
+            for (byte j : str) if (j == letter) count++;
+            
+            if (count <= max && count >= min) valid++;
+            if (str[min - 1] == letter ^ str[max - 1] == letter) valid2++;
         }
-        System.out.println(total - 200);
-        System.out.println(total2);
-        
+        System.out.println(valid);
+        System.out.println(valid2);
     }
+
     static String next() throws IOException {
         while (in == null || !in.hasMoreTokens())
             in = new StringTokenizer(br.readLine());
         return in.nextToken();
     }
+
     static long readLong() throws IOException {
         return Long.parseLong(next());
     }
+
     static int readInt() throws IOException {
         return Integer.parseInt(next());
     }
+
     static double readDouble() throws IOException {
         return Double.parseDouble(next());
     }
+
     static char readChar() throws IOException {
         return next().charAt(0);
     }
+
     static String readLine() throws IOException {
         return br.readLine();
     }
