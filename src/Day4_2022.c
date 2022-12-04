b927a4906b3ea2aca6cd0a1ace1ef72772a5cd4e
Jonathan Wu <jonathan.wu3@outlook.com>
2022-12-04 00:49:37 -0500

    Create Day4_2022.c

---
+++ src/Day4_2022.c
@@ -0,0 +1,4 @@
+main(a,b,c,d,e){while(scanf("%d-%d,%d-%d",&a,&b,&c,&d))e+=a<=c&&b>=d||c<=a&&d>=b;printf("%d",e);}
+
+// This is a golf for part 1
+// tfw entire C program is less bytes than reading input in Python
