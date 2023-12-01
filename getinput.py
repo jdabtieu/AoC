d706c4f9a6b700b53446a25b096c2df3b490591e
Jonathan Wu <jonathan.wu3@outlook.com>
2023-11-30 23:54:45 -0500

    2023 setup

---
+++ getinput.py
@@ -0,0 +1,15 @@
+import requests
+from datetime import datetime
+
+with open("session.key", "r") as f:
+  session_key = f.read().strip()
+
+year = datetime.today().year
+month = datetime.today().month
+day = datetime.today().day
+
+assert(month == 12)
+
+data = requests.get(f'https://adventofcode.com/{year}/day/{day}/input', cookies={'session': session_key}).text
+with open(f"in/Day{day}_{year}.in", "w") as f:
+  f.write(data)
